54c5c60063e523f4be5e3699fb7145886553d06c
Stefan <kh2ouija@yahoo.com>
2013-06-01 22:10:25 +0300

    remade hashing

--- transcode_heroes.rb
+++ transcode_heroes.rb
@@ -1,10 +1,21 @@
 require 'json'
+require 'active_support/core_ext/string/inflections.rb'
 require_relative 'valve_parser.rb'
+
 
 heroes = ValveParser::parse('npc_heroes.txt')['DOTAHeroes']
 heroes.delete 'npc_dota_hero_base'
 heroes.delete 'Version'
 
+heroes2 = {}
+heroes.keys.sort.each do |k|
+  v = heroes[k]
+  v['name'] = v['url'].titleize
+  heroes2[k[14..-1]] = v
+end
+
+
+
 File.open('heroes.js', 'w') do |f|
-  f.write "heroes = #{heroes.to_json}"
+  f.write "heroes = #{heroes2.to_json}"
 end
